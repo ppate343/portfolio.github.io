52cbddcd5c9771ed85cc2a7b0731b52b0d506bb4
Puja Patel <71097041+ppate343@users.noreply.github.com>
2025-01-02 11:37:12 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+document.addEventListener("DOMContentLoaded", () => {
+  particlesJS.load("particles-js", "particles/particles.json", function () {
+    console.log("callback - particles.js config loaded");
+  });
+
+  particlesJS.load(
+    "particles-contact",
+    "particles/particles.json",
+    function () {
+      console.log("callback - particles.js config loaded");
+    }
+  );
+
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector("nav ul");
+
+  // Ensure elements exist before adding event listeners
+  if (hamburger && navLinks) {
+    // Toggle the navigation menu on smaller screens
+    hamburger.addEventListener("click", () => {
+      navLinks.classList.toggle("active");
+    });
+  }
+
+
+  const tabs = document.querySelectorAll(".tablinks");
+  const tabLine = document.querySelector(".tabs-line");
+
+  // Function to switch between tabs
+  function openTab(evt, categoryName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+
+    // Hide all tab content
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+
+    // Remove "active" class from all tabs
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Display the selected tab content
+    document.getElementById(categoryName).style.display = "block";
+
+    // Add "active" class to the clicked tab
+    evt.currentTarget.className += " active";
+
+    // Move the hotpink line under the active tab
+    const activeTab = evt.currentTarget;
+    const tabWidth = activeTab.offsetWidth;
+    const tabPosition = activeTab.offsetLeft;
+    tabLine.style.width = `${tabWidth}px`;
+    tabLine.style.left = `${tabPosition}px`;
+  }
+
+  // Set up event listeners for each tab button
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", (event) => {
+      const categoryName = event.target
+        .getAttribute("onclick")
+        .match(/'(.*?)'/)[1];
+      openTab(event, categoryName);
+    });
+  });
+
+  // Set the default active tab to open when the page loads
+  if (tabs.length > 0) {
+    tabs[0].click();
+  }
+});
